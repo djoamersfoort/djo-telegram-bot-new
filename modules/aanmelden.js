b54d0c5b599d78c39d0d18778a1f39afd053ae1a
sverben <59171289+sverben@users.noreply.github.com>
2022-11-10 23:07:54 +0100

    fixed possible drifting for aanmelden scheduling

--- modules/aanmelden.js
+++ modules/aanmelden.js
@@ -9,42 +9,23 @@ class Aanmelden {
     this.bot.command('aanmeld_status', ctx => {
       if (!config.allowedUsers.includes(ctx.update.message.chat.id)) { return ctx.reply(templates.render('notAllowed')) }
 
-      this.announce().then()
+      this.announce(true).then()
     })
-    setTimeout(this.announce, this.timeLeft())
+    setInterval(() => this.announce(), 60 * 1000)
   }
 
-  isToday () {
+  isNow () {
     const current = new Date()
 
     if (current.getDay() !== config.aanmelden.day) return false
-    if (current.getHours() > config.aanmelden.hour) return false
-    if (current.getHours() < config.aanmelden.hour) return true
-    if (current.getMinutes() > config.aanmelden.minutes) return false
+    if (current.getHours() !== config.aanmelden.hour) return false
+    if (current.getMinutes() !== config.aanmelden.minutes) return false
 
     return true
   }
 
-  nextWeek () {
-    const date = new Date()
-    date.setDate(date.getDate() + ((7 - date.getDay() + config.aanmelden.day) % 7 || 7))
-
-    return date
-  }
-
-  timeLeft () {
-    const current = new Date()
-
-    const date = this.isToday() ? current : this.nextWeek()
-    date.setHours(config.aanmelden.hour)
-    date.setMinutes(config.aanmelden.minutes)
-
-    return date.getTime() - current.getTime()
-  }
-
-  async announce () {
-    setTimeout(this.announce, 7 * 24 * 60 * 60 * 1000)
-
+  async announce (ignoreTime = false) {
+    if (!ignoreTime && !this.isNow()) return
     let res
     try {
       res = await fetch('https://aanmelden.djoamersfoort.nl/api/v2/free')
